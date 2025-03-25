705fee67a40b2095a0782bf80267d2b374d9510a
p22194 <gianmarco.wyss@css.ch>
2025-03-25 14:34:36 +0100

    feat: storybook

---
+++ src/app/features/search/search.component.stories.ts
@@ -0,0 +1,33 @@
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+import { SearchComponent } from './search.component';
+import { storybookApplicationConfig } from '../../../../.storybook/storybook-application-config';
+import { translationsDe } from '../../../../mock-inputs/translations';
+import { mockData } from '../../../../mock-server/mock-data';
+
+const meta: Meta<SearchComponent> = {
+  title: 'Search Component',
+  component: SearchComponent,
+  args: {
+    translations: translationsDe,
+    serviceProviders: mockData,
+    insurance: 'premium',
+  },
+  argTypes: {
+    translations: {
+      control: { type: 'object' },
+    },
+    serviceProviders: {
+      control: { type: 'object' },
+    },
+    insurance: {
+      options: ['balance', 'premium'],
+      control: { type: 'radio' },
+    },
+  },
+  decorators: [applicationConfig(storybookApplicationConfig)],
+};
+
+export default meta;
+type Story = StoryObj<SearchComponent>;
+
+export const Primary: Story = {};
